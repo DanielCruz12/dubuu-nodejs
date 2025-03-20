29671f9c80e0433946357161c03f9da7055588fb
dandev <danieldubonxd@gmail.com>
2025-03-20 15:47:31 -0600

    start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "dantours",
         script: "npm",
-        args: "run dev",
+        args: "run start",
         env: {
           NODE_ENV: "development",
         },
