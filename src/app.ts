b934bfe7f8271f9b239ec1355a6eaa63017bb95d
dandev <danieldubonxd@gmail.com>
2025-03-24 19:32:44 -0600

    Add endpoint for Wompi payment link; handle API request and response with error management

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,33 @@ dotenv.config()
 //* Create an Express application
 const app = express()
 
-
-
 //* Middleware to parse JSON bodies
 app.use(cors())
 
 app.use(express.json())
 
 setupSwagger(app)
+
+app.post('/api/enlacePago', async (req, res) => {
+  try {
+    // Realiza la petición a la API de Wompi
+    const response = await fetch('https://api.wompi.sv/EnlacePago', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.WOMPI_API_KEY}`,
+      },
+      body: JSON.stringify(req.body),
+    })
+
+    const data = await response.json()
+    // Envía la respuesta al cliente
+    res.json(data)
+  } catch (error) {
+    console.error('Error en la solicitud de pago:', error)
+    res.status(500).json({ error: 'Error en la solicitud' })
+  }
+})
 
 //* Define routes
 app.use('/api/v1', apiRoutes)
