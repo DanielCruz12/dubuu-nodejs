21ecdfc3a0539fc49eb0762591920e30439e577a
dandev <danieldubonxd@gmail.com>
2025-04-03 15:57:26 -0600

    Refactor product schema: remove services column, rename product_service_id to services, and update related constraints; create product amenities and join tables

--- drizzle/0000_nebulous_pet_avengers.sql
+++ drizzle/0000_nebulous_pet_avengers.sql
@@ -23,51 +23,44 @@ CREATE TABLE "faqs" (
 	"product_id" uuid NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "product_categories" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(155) NOT NULL,
-	"description" text NOT NULL
-);
---> statement-breakpoint
-CREATE TABLE "product_services" (
+CREATE TABLE "product_amenities" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(155) NOT NULL,
 	"description" text NOT NULL,
 	"category_id" uuid NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "product_services_products" (
+CREATE TABLE "product_amenities_products" (
 	"product_id" uuid NOT NULL,
-	"product_service_id" uuid NOT NULL,
-	CONSTRAINT "product_services_products_product_id_product_service_id_pk" PRIMARY KEY("product_id","product_service_id")
+	"product_amenity_id" uuid NOT NULL,
+	CONSTRAINT "product_amenities_products_product_id_product_amenity_id_pk" PRIMARY KEY("product_id","product_amenity_id")
+);
+--> statement-breakpoint
+CREATE TABLE "product_categories" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(155) NOT NULL,
+	"description" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "products" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(155) NOT NULL,
+	"user_id" text NOT NULL,
 	"description" text NOT NULL,
 	"price" numeric(10, 2) NOT NULL,
-	"departure_point" text DEFAULT '' NOT NULL,
 	"address" text DEFAULT '' NOT NULL,
 	"country" varchar(100) NOT NULL,
-	"available_dates" timestamp with time zone[] DEFAULT '{}' NOT NULL,
-	"itinerary" text[] DEFAULT '{""}',
-	"highlight" text DEFAULT '' NOT NULL,
-	"included" text DEFAULT '' NOT NULL,
 	"is_approved" boolean DEFAULT false NOT NULL,
-	"max_people" integer NOT NULL,
-	"duration" integer NOT NULL,
 	"images" text[] DEFAULT '{""}',
-	"videos" text[] DEFAULT '{""}',
 	"files" text[] DEFAULT '{""}',
+	"videos" text[] DEFAULT '{""}',
 	"banner" text,
-	"product_service_id" uuid NOT NULL,
-	"product_category_id" uuid NOT NULL,
-	"target_product_audience_id" uuid NOT NULL,
-	"user_id" text NOT NULL,
 	"is_active" boolean DEFAULT true NOT NULL,
 	"average_rating" numeric(8, 2) DEFAULT '0',
 	"total_reviews" integer DEFAULT 0,
+	"target_product_audience_id" uuid NOT NULL,
+	"product_category_id" uuid NOT NULL,
+	"product_type_id" uuid NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
@@ -95,6 +88,17 @@ CREATE TABLE "target_product_audiences" (
 	"description" text NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "tours" (
+	"product_id" uuid PRIMARY KEY NOT NULL,
+	"departure_point" text DEFAULT '' NOT NULL,
+	"available_dates" timestamp with time zone[] NOT NULL,
+	"max_people" integer NOT NULL,
+	"itinerary" text[] DEFAULT '{""}',
+	"highlight" text DEFAULT '' NOT NULL,
+	"included" text DEFAULT '' NOT NULL,
+	"duration" integer NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" text PRIMARY KEY NOT NULL,
 	"username" varchar(255) NOT NULL,
@@ -116,16 +120,19 @@ ALTER TABLE "bookings" ADD CONSTRAINT "bookings_product_id_products_id_fk" FOREI
 ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "faqs" ADD CONSTRAINT "faqs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "faqs" ADD CONSTRAINT "faqs_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "product_services" ADD CONSTRAINT "product_services_category_id_product_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."product_categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "product_services_products" ADD CONSTRAINT "product_services_products_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "product_services_products" ADD CONSTRAINT "product_services_products_product_service_id_product_services_id_fk" FOREIGN KEY ("product_service_id") REFERENCES "public"."product_services"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "products" ADD CONSTRAINT "products_product_service_id_product_services_id_fk" FOREIGN KEY ("product_service_id") REFERENCES "public"."product_services"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "products" ADD CONSTRAINT "products_product_category_id_product_categories_id_fk" FOREIGN KEY ("product_category_id") REFERENCES "public"."product_categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "products" ADD CONSTRAINT "products_target_product_audience_id_target_product_audiences_id_fk" FOREIGN KEY ("target_product_audience_id") REFERENCES "public"."target_product_audiences"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "product_amenities" ADD CONSTRAINT "product_amenities_category_id_product_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."product_categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "product_amenities_products" ADD CONSTRAINT "product_amenities_products_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "product_amenities_products" ADD CONSTRAINT "product_amenities_products_product_amenity_id_product_amenities_id_fk" FOREIGN KEY ("product_amenity_id") REFERENCES "public"."product_amenities"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "products" ADD CONSTRAINT "products_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "products" ADD CONSTRAINT "products_target_product_audience_id_target_product_audiences_id_fk" FOREIGN KEY ("target_product_audience_id") REFERENCES "public"."target_product_audiences"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "products" ADD CONSTRAINT "products_product_category_id_product_categories_id_fk" FOREIGN KEY ("product_category_id") REFERENCES "public"."product_categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "products" ADD CONSTRAINT "products_product_type_id_product_types_id_fk" FOREIGN KEY ("product_type_id") REFERENCES "public"."product_types"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "ratings" ADD CONSTRAINT "ratings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "ratings" ADD CONSTRAINT "ratings_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tours" ADD CONSTRAINT "tours_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "users" ADD CONSTRAINT "users_role_id_user_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."user_roles"("id") ON DELETE no action ON UPDATE no action;
+
+
 
 -- Función para actualizar average_rating y total_reviews en la tabla products
 CREATE OR REPLACE FUNCTION update_product_rating() RETURNS TRIGGER AS $$
--- drizzle/0001_mushy_giant_man.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "products" RENAME COLUMN "product_service_id" TO "services";--> statement-breakpoint
-ALTER TABLE "products" DROP CONSTRAINT "products_product_service_id_product_services_id_fk";
